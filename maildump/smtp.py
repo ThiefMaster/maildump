00348222040682a496fe7661acd264f1d0d714c7
Porawit Poboonma <porawit.p@bizidea.co.th>
2016-08-30 12:15:58 +0700

    add EHLO supports

--- maildump/smtp.py
+++ maildump/smtp.py
@@ -9,6 +9,16 @@ from maildump.db import add_message
 log = Logger(__name__)
 
 
+class SMTPChannel(smtpd.SMTPChannel):
+    def smtp_EHLO(self, arg):
+        if not arg:
+            self.push('501 Syntax: HELO hostname')
+        elif self.__greeting:
+            self.push('503 Duplicate HELO/EHLO')
+        else:
+            self.push('250 %s' % self.__fqdn)
+
+
 class SMTPServer(smtpd.SMTPServer, object):
     def __init__(self, listener, handler):
         super(SMTPServer, self).__init__(listener, None)
@@ -16,6 +26,10 @@ class SMTPServer(smtpd.SMTPServer, object):
 
     def process_message(self, peer, mailfrom, rcpttos, data):
         return self._handler(sender=mailfrom, recipients=rcpttos, body=data)
+
+    def handle_accept(self):
+        conn, addr = self.accept()
+        channel = SMTPChannel(self, conn, addr)
 
 
 def smtp_handler(sender, recipients, body):
