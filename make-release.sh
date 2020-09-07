2704542823718b849f4452b2818dd3fa18ad945e
Adrian Moennich <adrian@planetcoding.net>
2020-09-07 22:40:19 +0200

    Add some dev tools

--- make-release.sh
+++ make-release.sh
@@ -5,7 +5,7 @@ rm -rf maildump/static/.webassets-cache/ maildump/static/assets/bundle.*
 # Build current assets
 webassets -m maildump.web build
 webassets -m maildump.web build --production
-# Create and update release
-python setup.py sdist upload
+# Create release wheel
+python setup.py bdist_wheel
 # Delete assets again, we don't need them anymore
 rm -rf maildump/static/.webassets-cache/ maildump/static/assets/bundle.*
