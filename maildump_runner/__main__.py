2d2d1da612e27c80eb1f99bbf76103f3eb622907
Adrian Moennich <adrian@planetcoding.net>
2020-09-07 20:59:56 +0200

    Fix typo

--- maildump_runner/__main__.py
+++ maildump_runner/__main__.py
@@ -37,9 +37,9 @@ def terminate_server(sig, frame):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--smtp-ip', default='127.0.0.1', metavar='IP', help='SMTP ip (default: 127.0.0.1)')
-    parser.add_argument('--smtp-port', default=1025, type=int, metavar='PORT', help='SMTP port (deault: 1025)')
+    parser.add_argument('--smtp-port', default=1025, type=int, metavar='PORT', help='SMTP port (default: 1025)')
     parser.add_argument('--http-ip', default='127.0.0.1', metavar='IP', help='HTTP ip (default: 127.0.0.1)')
-    parser.add_argument('--http-port', default=1080, type=int, metavar='PORT', help='HTTP port (deault: 1080)')
+    parser.add_argument('--http-port', default=1080, type=int, metavar='PORT', help='HTTP port (default: 1080)')
     parser.add_argument('--db', metavar='PATH', help='SQLite database - in-memory if missing')
     parser.add_argument('--htpasswd', metavar='HTPASSWD', help='Apache-style htpasswd file')
     parser.add_argument('-v', '--version', help='Display the version and exit', action='store_true')
